7a04771a0a11c809f4741f720f9f8290d8697d60
Tamas Beszedics <beszedicshun@gmail.com>
2022-11-20 23:39:53 +0100

    feat(PS-27): implement registration modal

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '../../test-utils';
 import Header from './Header';
+import userEvent from '@testing-library/user-event';
 
 describe('Header unit test', () => {
   it('Header should render searchbar with the correct placeholder', async () => {
@@ -31,4 +32,86 @@ describe('Header unit test', () => {
     expect(signUpBtn).toBeInTheDocument();
     expect(loginBtn).toBeInTheDocument();
   });
+
+  it('If Sign up button is clicked then the registration modal is opened', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(<Header />);
+
+    const signUpBtn = getByRole('button', {
+      name: /sign up/i,
+    });
+
+    await user.click(signUpBtn);
+
+    const registrationHeading = getByRole('heading', {
+      name: /create your account/i,
+    });
+
+    expect(registrationHeading).toBeInTheDocument();
+  });
+
+  it('The close button is clicked then the registration modal is disappeared', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(<Header />);
+
+    const signUpBtn = getByRole('button', {
+      name: /sign up/i,
+    });
+
+    await user.click(signUpBtn);
+
+    const registrationHeading = getByRole('heading', {
+      name: /create your account/i,
+    });
+
+    const closeBtn = getByRole('button', {
+      name: /close/i,
+    });
+
+    await user.click(closeBtn);
+
+    expect(registrationHeading).not.toBeInTheDocument();
+  });
+
+  it('The ESC button is clicked then the registration modal is disappeared', async () => {
+    const user = userEvent.setup();
+    const { getByRole } = render(<Header />);
+
+    const signUpBtn = getByRole('button', {
+      name: /sign up/i,
+    });
+
+    await user.click(signUpBtn);
+
+    const registrationHeading = getByRole('heading', {
+      name: /create your account/i,
+    });
+
+    await user.keyboard('[Escape]');
+
+    expect(registrationHeading).not.toBeInTheDocument();
+  });
+
+  /* it('Clicking outside of the registration modal then the registration modal is disappeared', async () => {
+    const user = userEvent.setup();
+    const { getByRole, getByTestId } = render(<Header />);
+
+    const signUpBtn = getByRole('button', {
+      name: /sign up/i,
+    });
+
+    await user.click(signUpBtn);
+
+    const registrationHeading = getByRole('heading', {
+      name: /create your account/i,
+    });
+
+    const registrationOverlay = getByTestId('registration_overlay');
+
+    console.log(registrationOverlay);
+
+    await user.click(registrationOverlay);
+
+    expect(registrationHeading).not.toBeInTheDocument();
+  }); */
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,57 +12,68 @@ import React, { useState } from 'react';
 
 import { SearchIcon } from '@chakra-ui/icons';
 import Logo from '../Logo/Logo';
+import Registration from '../Registration/Registration';
 
 const Header = () => {
+  const [showRegistrationModal, setShowRegistrationModal] = useState(false);
+
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-  const handleSignUpClick = () => console.log('Sign up is clicked');
+
+  const onSignUpClick = () => {
+    console.log('Sign up is clicked');
+    setShowRegistrationModal(!showRegistrationModal);
+  };
+
   const handleLoginClick = () => console.log('Login is clicked');
 
   return (
-    <HStack
-      bg="white"
-      w="100%"
-      minH="80px"
-      p={4}
-      color="black"
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      boxShadow="sm"
-      wrap="wrap"
-    >
-      <Box minW="200px">
-        <Logo />
-      </Box>
-      <Box w="510px">
-        <InputGroup>
-          <Input
-            type="text"
-            placeholder="Search playground..."
-            focusBorderColor="teal.600"
-          />
-          <InputRightElement
-            pointerEvents="none"
-            // eslint-disable-next-line react/no-children-prop
-            children={<SearchIcon color="#9A9A9A" />}
-          />
-        </InputGroup>
-      </Box>
-      {isUserLoggedIn ? (
-        <Box>
-          <Avatar name="Test User" bg="teal.400" />
+    <>
+      <HStack
+        bg="white"
+        w="100%"
+        minH="80px"
+        p={4}
+        color="black"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        boxShadow="sm"
+        wrap="wrap"
+      >
+        <Box minW="200px">
+          <Logo />
         </Box>
-      ) : (
-        <Stack spacing={4} direction="row" align="center">
-          <Button colorScheme="teal" onClick={handleSignUpClick}>
-            Sign up
-          </Button>
-          <Button colorScheme="teal" onClick={handleLoginClick}>
-            Login
-          </Button>
-        </Stack>
-      )}
-    </HStack>
+        <Box w="510px">
+          <InputGroup>
+            <Input
+              type="text"
+              placeholder="Search playground..."
+              focusBorderColor="teal.600"
+            />
+            <InputRightElement
+              pointerEvents="none"
+              // eslint-disable-next-line react/no-children-prop
+              children={<SearchIcon color="#9A9A9A" />}
+            />
+          </InputGroup>
+        </Box>
+        {isUserLoggedIn ? (
+          <Box>
+            <Avatar name="Test User" bg="teal.400" />
+          </Box>
+        ) : (
+          <Stack spacing={4} direction="row" align="center">
+            <Button colorScheme="teal" onClick={onSignUpClick}>
+              Sign up
+            </Button>
+            <Button colorScheme="teal" onClick={handleLoginClick}>
+              Login
+            </Button>
+          </Stack>
+        )}
+      </HStack>
+      {showRegistrationModal && <Registration isOpen onClose={onSignUpClick} />}
+    </>
   );
 };
 
